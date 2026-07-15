5f77bd1c2a429d648a0b5df7098db8b0d6fac887
Sadra <mmdsadra88@gmail.com>
2026-07-15 22:21:32 +0330

    Set up react hook form

--- app/admin/majors/new/page.tsx
+++ app/admin/majors/new/page.tsx
@@ -1,20 +1,34 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
 import { Field, FieldGroup, FieldLabel, FieldLegend, FieldSet } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useForm } from "react-hook-form" ;
+
+interface FormData {
+  title : string;
+  description : string;
+}
 
 const NewMajorsPage = () => {
+  const { register , handleSubmit } = useForm<FormData>();
+
   return (
-    <form className='w-xl'>
+    <form className='w-xl' onSubmit={handleSubmit((data) => console.log(data))}>
       <FieldSet>
         <FieldLegend>ساخت رشته</FieldLegend>
         <FieldGroup>
           <Field>
             <FieldLabel htmlFor="title">عنوان رشته</FieldLabel>
-            <Input id="title" className='w-full' />
+            <Input id="title" className='w-full' { ...register('title') } />
           </Field>
           <Field>
             <FieldLabel htmlFor="description">توضیحات رشته</FieldLabel>
-            <Textarea />
+            <Textarea { ...register('description') } />
+          </Field>
+          <Field>
+            <Button className='cursor-pointer' type="submit">ثبت</Button>
           </Field>
         </FieldGroup>
       </FieldSet>
