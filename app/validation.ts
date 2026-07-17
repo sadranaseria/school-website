32fd2c4321f581bf213c48db03f051656a4dc330
Sadra <mmdsadra88@gmail.com>
2026-07-17 21:46:45 +0330

    Remvoe limit of description

--- app/validation.ts
+++ app/validation.ts
@@ -2,7 +2,7 @@ import z from 'zod/v4';
 
 export const createMajorSchema = z.object({
   title : z.string().min(1 , 'عنوان رشته الزامی است'),
-  description : z.string().min(1 , 'توضیحات الزامی است').max(225)
+  description : z.string().min(1 , 'توضیحات الزامی است')
 })
 
 export type CreateFormData = z.infer<typeof createMajorSchema>;
