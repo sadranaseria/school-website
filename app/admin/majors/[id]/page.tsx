a9e2e9de93eaaf539f9654cb74f26b7600b7425f
Sadra <mmdsadra88@gmail.com>
2026-07-17 21:44:29 +0330

    Add markdown in detalis major component

--- app/admin/majors/[id]/page.tsx
+++ app/admin/majors/[id]/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Card } from '@/components/ui/card';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { prisma } from '@/prisma/client';
+import MarkDown from 'react-markdown';
 
-const MajorDetalisPage = () => {
+const MajorDetalisPage = async ({ params } : { params : Promise<{ id : string }>}) => {
+    const { id } = await params;
+    const major = await prisma.major.findUnique({
+        where : { id : parseInt(id) }
+    })
   return (
-    <div>MajorDetalisPage</div>
+    <div className="flex flex-col gap-4 w-full max-w-3xl h-100">
+        <h1>{major?.title}</h1>
+        <Card className='prose p-4'>
+            <ScrollArea>
+            <MarkDown>{major?.description}</MarkDown>
+            </ScrollArea>
+        </Card>
+    </div>
   )
 }
 
