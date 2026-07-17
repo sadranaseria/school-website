954354e9200bc9ebceb1d01d423a1ad7ff639114
Sadra <mmdsadra88@gmail.com>
2026-07-17 20:42:11 +0330

    Refactor : uptomize the strackture

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "@/app/appSidebar";
+import { AppSidebar } from "@/app/admin/appSidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { ReactNode } from "react";
 
