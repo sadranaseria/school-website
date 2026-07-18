94a0c2bda7b95a97a9b3a9ca3562cb1946a6c5b5
Sadra <mmdsadra88@gmail.com>
2026-07-18 17:19:20 +0330

    Create a loading page for edit form

---
+++ app/admin/majors/[id]/edit/loading.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import React from 'react'
+
+const EditLoadingPage = () => {
+  return (
+    <div className="flex w-full max-w-xl min-h-xl flex-col gap-18">
+      <div className="flex flex-col gap-3">
+        <Skeleton className="h-4 w-20 bg-gray-200" />
+        <Skeleton className="h-8 w-full bg-gray-200" />
+      </div>
+      <div className="flex flex-col gap-3">
+        <Skeleton className="h-4 w-24 bg-gray-200" />
+        <Skeleton className="h-70 w-full bg-gray-200" />
+      </div>
+      <Skeleton className="h-8 w-full bg-gray-200" />
+    </div>
+  )
+}
+
+export default EditLoadingPage
