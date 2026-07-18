85a267ab7d8683cda9fcfbae67e94a602fd70a52
Sadra <mmdsadra88@gmail.com>
2026-07-18 15:56:56 +0330

    Refactor : change the type of description to Text

--- app/admin/majors/[id]/page.tsx
+++ app/admin/majors/[id]/page.tsx
@@ -1,21 +1,27 @@
-import { Card } from '@/components/ui/card';
-import { ScrollArea } from '@/components/ui/scroll-area';
-import { prisma } from '@/prisma/client';
-import MarkDown from 'react-markdown';
+import { Card } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { prisma } from "@/prisma/client";
+import MarkDown from "react-markdown";
 
-const MajorDetalisPage = async ({ params } : { params : Promise<{ id : string }>}) => {
-    const { id } = await params;
-    const major = await prisma.major.findUnique({
-        where : { id : parseInt(id) }
-    })
+const MajorDetalisPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
+  const major = await prisma.major.findUnique({
+    where: { id: parseInt(id) },
+  });
   return (
     <div className="flex flex-col gap-4 w-full max-w-3xl h-100">
-        <h1>{major?.title}</h1>
-        <Card className='max-w-3xl min-h-100 prose p-4 wrap-anywhere'>
-            <MarkDown>{major?.description}</MarkDown>
+      <h1>{major?.title}</h1>
+      <ScrollArea className="h-100 w-full rounded-md">
+        <Card className="max-w-3xl min-h-100 prose p-4 wrap-anywhere">
+          <MarkDown>{major?.description}</MarkDown>
         </Card>
+      </ScrollArea>
     </div>
-  )
-}
+  );
+};
 
-export default MajorDetalisPage
+export default MajorDetalisPage;
---
+++ prisma/migrations/20260718122413_change_the_description_type_to_text/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Major" ALTER COLUMN "description" SET DATA TYPE TEXT;
