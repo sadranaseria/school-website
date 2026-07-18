2df1ae0c2ebe04c54f44ecad0bf39cb591093138
Sadra <mmdsadra88@gmail.com>
2026-07-18 16:31:50 +0330

    Create a edit file

---
+++ app/admin/majors/[id]/edit/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditMajorPage = () => {
+  return (
+    <div>EditMajorPage</div>
+  )
+}
+
+export default EditMajorPage
--- app/admin/majors/[id]/page.tsx
+++ app/admin/majors/[id]/page.tsx
@@ -4,6 +4,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { prisma } from "@/prisma/client";
 import MarkDown from "react-markdown";
 import DeleteMajorButton from "./_components/DeleteMajorButton";
+import Link from "next/link";
 
 const MajorDetalisPage = async ({
   params,
@@ -18,7 +19,12 @@ const MajorDetalisPage = async ({
     <div className="flex flex-col gap-4 w-full max-w-3xl h-100">
       <div className="flex justify-between items-center">
         <h1>{major?.title}</h1>
-        <DeleteMajorButton majorId={parseInt(id)} />
+        <div className="flex items-center gap-4">
+          <DeleteMajorButton majorId={parseInt(id)} />
+          <Button className='bg-violet-200 hover:bg-violet-300'>
+            <Link href={`/admin/majors/${id}/edit`} className='text-violet-600'>ویرایش</Link>
+          </Button>
+        </div>
       </div>
       <Card className="max-w-3xl min-h-100 prose p-4 wrap-anywhere">
         <ScrollArea className="h-100 w-full rounded-md">
