2344df313a6dce93a7192fcf49e7aad3103f0e1f
Sadra <mmdsadra88@gmail.com>
2026-07-20 20:33:04 +0330

    Implement signing out

--- app/admin/AuthStatus.tsx
+++ app/admin/AuthStatus.tsx
@@ -1,15 +1,9 @@
-"use client"
+"use client";
 
-import {
-  LogOutIcon
-} from "lucide-react"
+import { LogOutIcon } from "lucide-react";
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -17,31 +11,42 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
+} from "@/components/ui/dropdown-menu";
+import {
+  LogoutLink,
+  useKindeBrowserClient,
+} from "@kinde-oss/kinde-auth-nextjs";
 
 export default function AuthStatus() {
   const { user } = useKindeBrowserClient();
 
-  if(!user) return
+  if (!user) return;
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger render={<Button variant="ghost" size="icon" className="rounded-full"><Avatar>
-          <AvatarImage src={`https://ui-avatars.com/api/?name=${user.given_name}`} alt="Admin" />
-          <AvatarFallback>A</AvatarFallback>
-        </Avatar></Button>} />
-      <DropdownMenuContent align="end" className='w-50'>
+      <DropdownMenuTrigger
+        render={
+          <Button variant="ghost" size="icon" className="rounded-full">
+            <Avatar>
+              <AvatarImage
+                src={`https://ui-avatars.com/api/?name=${user.given_name}`}
+                alt="Admin"
+              />
+              <AvatarFallback>AD</AvatarFallback>
+            </Avatar>
+          </Button>
+        }
+      />
+      <DropdownMenuContent align="end" className="w-50">
         <DropdownMenuGroup>
           <DropdownMenuItem>{user.email}</DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <LogOutIcon />
-          <LogoutLink>خروج</LogoutLink>
+        <DropdownMenuItem variant="destructive">
+            <LogOutIcon />
+            <LogoutLink>خروج</LogoutLink>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
-
