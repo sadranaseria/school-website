aef5c6c6d020697f12f32036c8d1c518dff81c22
Sadra <mmdsadra88@gmail.com>
2026-07-21 03:16:36 +0330

    Create a demo component of latest articels

---
+++ app/admin/LatestArticels.tsx
@@ -0,0 +1,16 @@
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+
+const LatestArticels = () => {
+    // get 5 lates data from prisma and use them in this cmoponent 
+  return (
+    <Table>
+        <TableBody>
+            <TableRow>
+                <TableCell>Something</TableCell>
+            </TableRow>
+        </TableBody>
+    </Table>
+  )
+}
+
+export default LatestArticels
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,11 @@
+import LatestArticels from "./LatestArticels";
 import Summery from "./Summery";
 
 const Dashboard = () => {
   return (
     <div className="w-300 h-screen mt-30">
       <Summery />
+      <LatestArticels />
     </div>
   );
 };
