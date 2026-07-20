e76704517a5856120ea40c2c3328a25beb91d29b
Sadra <mmdsadra88@gmail.com>
2026-07-20 21:32:27 +0330

    Add a logo

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,21 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import logo from "../public/icon0.svg";
 
 const NavBar = () => {
-  const links: { lebel: string; value: string; href: string }[] = [
-    { lebel: "رشته ها", value: "Majros", href: "#majors" },
-    { lebel: "گالری آثار", value: "Gallery", href: "#gallery" },
-    { lebel: "سوالات متداول", value: "Questions", href: "#questions" },
-    { lebel: "قدم های ثبت نام", value: "Roadmap", href: "#roadmap" },
-  ];
 
   return (
-    <nav className="bg-white flex fixed top-5">
-      <div className=""></div>
+    <nav className="bg-white flex items-center gap-6 fixed top-5">
+      <Image width={60} src={logo} alt="logo" />
       <ul className="flex gap-5">
         {links.map((link) => (
           <li key={link.value}>
-            <Link href={link.href}>{link.lebel}</Link>
+            <Link href={link.href}>
+              {link.lebel}
+            </Link>
           </li>
         ))}
       </ul>
@@ -23,4 +22,10 @@ const NavBar = () => {
   );
 };
 
+const links: { lebel: string; value: string; href: string }[] = [
+  { lebel: "رشته ها", value: "Majros", href: "#majors" },
+  { lebel: "گالری آثار", value: "Gallery", href: "#gallery" },
+  { lebel: "سوالات متداول", value: "Questions", href: "#questions" },
+  { lebel: "قدم های ثبت نام", value: "Roadmap", href: "#roadmap" },
+];
 export default NavBar;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images : {
+    localPatterns : [
+      {
+        pathname : './public/icon0.svg',
+        search : ''
+      }
+    ]
+  }
 };
 
 export default nextConfig;
