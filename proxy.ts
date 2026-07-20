6f9fda923b4e7511631d0fd560d80988f0c2d86e
Sadra <mmdsadra88@gmail.com>
2026-07-20 07:04:23 +0330

    Implement autonecation

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,11 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
 export default withAuth({
-    loginPage : '/api/auth/login'
+  loginPage : '/api/auth/login'
 });
 
 export const config = {
-  matcher: ['/admin/:path*'],
+  matcher: [
+    '/admin/:path*',
+  ],
 }
