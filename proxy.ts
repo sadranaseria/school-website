5b1a66165e7c46de8cdf62f7ce5410d50d5e1449
Sadra <mmdsadra88@gmail.com>
2026-07-19 20:05:44 -0600

    protect the admin page

---
+++ proxy.ts
@@ -0,0 +1,9 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export default withAuth({
+    loginPage : '/api/auth/login'
+});
+
+export const config = {
+  matcher: ['/admin'],
+}
