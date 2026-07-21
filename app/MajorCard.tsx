2b5a0e4c9049cff2d0920e8a2ddd8fb2b1ed158d
Sadra <mmdsadra88@gmail.com>
2026-07-21 20:13:57 +0330

    Add a show more less button

--- app/MajorCard.tsx
+++ app/MajorCard.tsx
@@ -1,13 +1,14 @@
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
 import { Major } from "@/lib/generated/prisma/client";
 import Markdown from "react-markdown";
+import ShowMaoreLess from "./components/ShowMaoreLess";
 
 const MajorCard = ({ major }: { major: Major }) => {
   return (
     <Card className="p-4 ring-0">
       <CardTitle className="text-6xl text-primary">{major.title}</CardTitle>
       <CardDescription className="prose">
-        <Markdown>{major.description}</Markdown>
+          <ShowMaoreLess text={major.description} />
       </CardDescription>
     </Card>
   );
