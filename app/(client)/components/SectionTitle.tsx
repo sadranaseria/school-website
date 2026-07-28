1462fbf8647589ab8134bb30a2a6e4587cb031a9
Sadra <mmdsadra88@gmail.com>
2026-07-28 19:43:56 +0330

    Add hover to title

--- app/(client)/components/SectionTitle.tsx
+++ app/(client)/components/SectionTitle.tsx
@@ -11,11 +11,11 @@ const SectionTitle = ({ title, id ,href }: Props) => {
   if (!href) return null;
 
   return (
-    <div className="w-full mx-auto pt-14 flex items-center gap-2" id={id}>
+    <div className="group pt-14 flex items-center justify-center gap-2" id={id}>
       <h2 className="text-center text-primary text-4xl my-20">
         {title}
       </h2>
-        <Link href={href}>
+        <Link href={href} className="hidden group-hover:block">
           <LuLink className="size-5 stroke-primary" />
         </Link>
     </div>
