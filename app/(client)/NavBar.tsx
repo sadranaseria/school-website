55d6d55852a473db5cf1b166308e656c02ff4b43
sadra <mmdsadra88@gmail.com>
2026-08-30 13:10:27 -0400

    Create sidebar on home page

--- app/(client)/NavBar.tsx
+++ app/(client)/NavBar.tsx
@@ -6,13 +6,34 @@ import logo from "../../public/icon0.svg";
 import useActiveLink from "./store";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 const NavBar = () => {
+  const [isShow, setShow] = useState(false);
+  console.log(isShow);
   const linkId = useActiveLink((state) => state.linkId);
   return (
     <nav className="p-2 flex flex-row-reverse md:flex-row items-center justify-between md:justify-normal gap-2 fixed top-5 left-2 right-2 rounded-xl bg-white shadow-2xl z-10">
-      <Image width={50} height={50} src={logo} alt="logo" className="size-10 md:size-14" />
-      <ul className="hidden md:flex gap-5">
+      <Image
+        width={50}
+        height={50}
+        src={logo}
+        alt="logo"
+        className="size-10 md:size-14"
+      />
+      {isShow && (
+        <div
+          className="fixed top-0 right-0 left-0 bottom-0 md:hidden"
+          onClick={() => setShow(false)}
+        ></div>
+      )}
+      <ul
+        className={cn(
+          "w-100 fixed md:static top-0 bottom-0 bg-white p-6 space-y-4 md:space-y-0 md:flex gap-5 md:p-0 rounded-l-xl transition-all",
+          isShow ? "right-0" : "-right-100",
+        )}
+      >
         {links.map((link) => (
           <li key={link.value}>
             <Link
@@ -32,7 +53,7 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
-      <Button variant='ghost'>
+      <Button variant="ghost" onClick={() => setShow(true)}>
         <RxHamburgerMenu className="size-5 md:hidden cursor-pointer" />
       </Button>
     </nav>
