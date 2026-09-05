0acbb9c5f115118b40911d837b14c91abb2fa606
sadra <mmdsadra88@gmail.com>
2026-09-05 09:42:13 -0400

    Delete uploaded images

--- app/(admin)/admin/_components/ImageDropzone.tsx
+++ app/(admin)/admin/_components/ImageDropzone.tsx
@@ -5,6 +5,7 @@ import { UploadDropzone } from "@/utils/uploadthing";
 import Image from "next/image";
 import { useState } from "react";
 import { HiX } from "react-icons/hi";
+import { deleteImage } from "../action";
 
 type ImageType = Array<{ url: string; key: string }>;
 
@@ -16,21 +17,29 @@ interface Props {
 const ImageDropzone = ({ onChange, value }: Props) => {
   const [images, setImages] = useState<ImageType>(value ?? []);
 
+  const handleDelete = async (key: string) => {
+    const { success, message } = await deleteImage(key);
+    if (success)
+      setImages(images.filter(img => img.key !== key));
+    else
+      console.log(message);
+  };
+
   return (
     <>
       <UploadDropzone
         className="hover:border-blue-500 cursor-pointer"
         endpoint="imageUploader"
         onClientUploadComplete={(res) => {
-          const newImages = res.map(r => ({ url: r.ufsUrl, key: r.key }));
-          
-          setImages((prev) => ([...prev, ...newImages]));
+          const newImages = res.map((r) => ({ url: r.ufsUrl, key: r.key }));
+
+          setImages((prev) => [...prev, ...newImages]);
 
           onChange?.([
             ...images.map(({ url, key }) => ({ url, key })),
-            ...newImages
-          ])
-          
+            ...newImages,
+          ]);
+
           console.log(images);
         }}
         onUploadError={(error: Error) => {
@@ -52,7 +61,13 @@ const ImageDropzone = ({ onChange, value }: Props) => {
               height={200}
               loading="eager"
             />
-            <Button variant='destructive' className='absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity'><HiX /></Button>
+            <Button
+              onClick={() => handleDelete(image.key)}
+              variant="destructive"
+              className="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity"
+            >
+              <HiX />
+            </Button>
           </div>
         ))}
       </div>
--- app/(admin)/admin/action.ts
+++ app/(admin)/admin/action.ts
@@ -2,7 +2,19 @@
 
 import { prisma } from "@/prisma/client";
 import { cache } from "react";
+import { utapi } from "@/server/uploadthing";
 
-export const fetchMajor = cache((majorId: number) =>
+export const fetchMajor = cache(async (majorId: number) =>
   prisma.major.findUnique({ where: { id: majorId } }),
 );
+
+export async function deleteImage(key: string) {
+  try {
+    await utapi.deleteFiles([key]);
+    return { success : true }
+  } catch (error) {
+    console.log(error);
+    return { success : false , message : 'آپلود انجام نشد' }
+  }
+  
+}
