a713a4865c36ab0653866c3c2ed9bc105bf2b5c5
sadra <mmdsadra88@gmail.com>
2026-09-05 11:22:44 -0400

    Show news details

--- app/(admin)/admin/news/[id]/page.tsx
+++ app/(admin)/admin/news/[id]/page.tsx
@@ -1,6 +1,32 @@
-const NewsDetailsPage = () => {
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { prisma } from "@/prisma/client";
+import Image from "next/image";
+import Markdown from "react-markdown";
+
+const NewsDetailsPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+
+  const news = await prisma.news.findUnique({
+    where: { id: parseInt(id) },
+    include : { images : true }
+  })
+
+  if (!news) return null;
+  
   return (
-    <>NewsDetailsPage</>
+    <Card className="w-7xl h-full">
+      <CardHeader className="w-4xl mx-auto my-14 flex justify-between items-center">
+        <CardTitle className="text-3xl">{news.title}</CardTitle>
+        <div>
+          {news.images.map(image => (
+            <Image key={image.id} src={image.url} alt={`Image for news ${image.newsId}`} width={200} height={200} className="w-100 object-cover rounded-4xl" />
+          ))}
+        </div>
+      </CardHeader>
+      <CardContent className="w-5xl mx-auto">
+        <Markdown>{news.description}</Markdown>
+      </CardContent>
+    </Card>
   )
 }
 
