e3b614daf1bcc182f38cf7546305acb22a2ac01a
sadra <mmdsadra88@gmail.com>
2026-09-05 10:48:00 -0400

    Make table better

--- app/(admin)/admin/news/page.tsx
+++ app/(admin)/admin/news/page.tsx
@@ -1,13 +1,31 @@
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { prisma } from "@/prisma/client";
+import Image from "next/image";
 import Link from "next/link";
 import { NewsWithImages } from "./types";
+import Markdown from "react-markdown";
 
-const NewsPage = () => {
+const NewsPage = async () => {
+  const news = await prisma.news.findMany({
+    include: {
+      images: true,
+    },
+  });
+
+  if (!news) return null;
+
   return (
-    <div className="w-6xl">
-      <Button className='mb-4'>
+    <div className="w-4xl">
+      <Button className="mb-4">
         <Link href="/admin/news/new">جدید</Link>
       </Button>
       <ScrollArea className="h-100 rounded-md border">
@@ -15,10 +33,30 @@ const NewsPage = () => {
           <TableHeader>
             <TableRow>
               {columns.map((column) => (
-                <TableHead className="text-right" key={column.value}>{column.label}</TableHead>
+                <TableHead className="text-right" key={column.value}>
+                  {column.label}
+                </TableHead>
               ))}
             </TableRow>
           </TableHeader>
+          <TableBody>
+            {news.map((n) => (
+              <TableRow key={n.id}>
+                <TableCell className="w-12">{n.id}</TableCell>
+                <TableCell className="w-40">
+                  <Image
+                    src={n.images[0].url}
+                    alt={`Image for news ${n.images[0].newsId}`}
+                    width={200}
+                    height={200}
+                    className="rounded-xl object-cover"
+                  />
+                </TableCell>
+                <TableCell>{n.title}</TableCell>
+                <TableCell><Markdown>{n.description.substring(0 ,20)}</Markdown></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
       </ScrollArea>
     </div>
