4b4fe35dbfb2d942716a9d17d19baf6e0fe37aab
sadra <mmdsadra88@gmail.com>
2026-09-05 11:39:39 -0400

    Handle loading in delete button component

--- app/(admin)/admin/news/[id]/DeleteButton.tsx
+++ app/(admin)/admin/news/[id]/DeleteButton.tsx
@@ -1,25 +1,39 @@
-'use client';
+"use client";
 
 import { Button } from "@/components/ui/button";
-import { deleteNews } from "../actions";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { deleteNews } from "../actions";
+import { Spinner } from "@/components/ui/spinner";
 
 const DeleteButton = ({ newsId }: { newsId: number }) => {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const handleDelete = async (newsId: number) => {
     try {
+      setLoading(true);
       await deleteNews(newsId);
     } catch (error) {
       console.log(error);
     } finally {
-      router.push('/admin/news');
+      setLoading(false);
+      router.push("/admin/news");
     }
-  }
-  
+  };
+
   return (
-    <Button variant='destructive' onClick={() => handleDelete(newsId)}>حذف خبر</Button>
-  )
-}
+    <Button variant='destructive' className="w-full mt-4" type="submit" onClick={() => handleDelete(newsId)}>
+      {loading ? (
+        <div className="flex items-center gpa-4">
+          <Spinner />
+          در حال حذف خبر
+        </div>
+      ) : (
+        "حذف خبر"
+      )}
+    </Button>
+  );
+};
 
 export default DeleteButton;
