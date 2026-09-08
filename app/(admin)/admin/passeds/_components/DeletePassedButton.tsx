5da65dd7f79b4527fbc70cd6eae83c3d7a02c614
sadra <mmdsadra88@gmail.com>
2026-09-08 11:36:55 -0400

    Handle delete passed loading

--- app/(admin)/admin/passeds/_components/DeletePassedButton.tsx
+++ app/(admin)/admin/passeds/_components/DeletePassedButton.tsx
@@ -1,23 +1,29 @@
-'use client';
+"use client";
 
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import { useState } from "react";
 import { HiTrash } from "react-icons/hi2";
 import { deletePassed } from "../actions";
 
 const DeletePassedButton = ({ passedId }: { passedId: number }) => {
+  const [isLoading, setLoading] = useState(false);
 
   const handleDelete = async () => {
     try {
+      setLoading(true);
       await deletePassed(passedId);
     } catch (error) {
       console.log(error);
     } finally {
-      
+      setLoading(false);
     }
-  }
+  };
   return (
-    <Button variant='destructive' onClick={handleDelete}><HiTrash className="size-5" /></Button>
-  )
-}
+    <Button variant="destructive" onClick={handleDelete}>
+      {isLoading ? <Spinner /> : <HiTrash className="size-5" />}
+    </Button>
+  );
+};
 
 export default DeletePassedButton;
