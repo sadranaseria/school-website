6e46a3b93ab076bde320e940ae797d9d531e27f7
sadra <mmdsadra88@gmail.com>
2026-09-09 11:23:36 -0400

    Add some styles to active links

--- app/(admin)/admin/NavBar.tsx
+++ app/(admin)/admin/NavBar.tsx
@@ -3,22 +3,26 @@
 import classNames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { RiGalleryFill } from "react-icons/ri";
+import { PiStudentBold } from "react-icons/pi";
+import { HiOutlineNewspaper } from "react-icons/hi2";
+import { FaUniversity } from "react-icons/fa";
+import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
 
 const NavBar = () => {
   const pathName = usePathname();
 
   return (
-    <ul className="space-y-1">
+    <ul className="mt-3">
       {sidebarItems.map((sidebarItem) => (
-        <li key={sidebarItem.value}>
-          <Link
-            href={sidebarItem.href}
-            className={classNames({
-              "text-zinc-950": pathName === sidebarItem.href,
-              "text-zinc-700 hover:text-zinc-800":
-                pathName !== sidebarItem.href,
-            })}
-          >
+        <li key={sidebarItem.value} className={cn(
+          'flex items-center gap-5 p-2 rounded-xl hover:text-secondary',
+          pathName === sidebarItem.href && 'bg-blue-superlight'
+        )}>
+          {sidebarItem.icon}
+          <Link href={sidebarItem.href}>
             {sidebarItem.lable}
           </Link>
         </li>
@@ -27,12 +31,12 @@ const NavBar = () => {
   );
 };
 
-const sidebarItems: { lable: string; value: string; href: string }[] = [
-  { lable: "داشبورد", value: "dashboard", href: "/admin" },
-  { lable: "گالری", value: "gallery", href: "/admin/gallery" },
-  { lable: "رشته ها", value: "majors", href: "/admin/majors" },
-  { lable: "اخبار", value: "news", href: "/admin/news" },
-  { lable: "قبول شدگان دانشگاه", value: "passUnivercity", href: "/admin/passeds" },
+const sidebarItems: { lable: string; value: string; href: string , icon : ReactNode }[] = [
+  { lable: "داشبورد", value: "dashboard", href: "/admin" , icon : <AiOutlineDashboard className="size-4" />},
+  { lable: "گالری", value: "gallery", href: "/admin/gallery" , icon : <RiGalleryFill className="size-4" />},
+  { lable: "رشته ها", value: "majors", href: "/admin/majors" , icon : <PiStudentBold className="size-4" />},
+  { lable: "اخبار", value: "news", href: "/admin/news" , icon : <HiOutlineNewspaper className="size-4" />},
+  { lable: "قبول شدگان دانشگاه", value: "passUnivercity", href: "/admin/passeds" , icon : <FaUniversity className="size-4" />},
 ];
 
 export default NavBar;
