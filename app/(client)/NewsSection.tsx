aab0993c52e0c2d2c842031c966effcfa73c4268
sadra <mmdsadra88@gmail.com>
2026-09-12 11:49:40 -0400

    Add some style in news sectoin

--- app/(client)/NewsSection.tsx
+++ app/(client)/NewsSection.tsx
@@ -1,7 +1,14 @@
 import { prisma } from "@/prisma/client";
 import ObserverProvider from "./components/ObserverProvider";
 import SectionTitle from "./components/SectionTitle";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
@@ -14,17 +21,30 @@ const NewsSection = async () => {
     <>
       <ObserverProvider id="news">
         <SectionTitle title="اخبار" href="#news" />
-        <div className="flex gap-5">
-          {news.map(n => (
-            <Card key={n.id} className="max-w-100 w-full p-4 space-y-4">
-              <CardHeader>
-                <Image src={n.images[0].url} alt={`Image of news ${n.images[0].newsId}`} width={500} height={500} className="w-full object-cover rounded-xl" />
+        <div className="flex gap-5 my-20">
+          {news.map((n) => (
+            <Card
+              key={n.id}
+              className="ring-0 max-w-90 w-full min-h-120 h-full p-0 space-y-4 flex flex-col justify-between relative rounded-4xl hover:cursor-pointer group"
+            >
+              <CardHeader className="p-0">
+                <Image
+                  src={n.images[0].url}
+                  alt={`Image of news ${n.images[0].newsId}`}
+                  width={500}
+                  height={500}
+                  className="top-0 right-0 left-0 h-58 object-cover"
+                />
               </CardHeader>
-              <div className="p-2">
-                <CardTitle>{n.title}</CardTitle>
-                <CardDescription>{n.description}</CardDescription>
-              </div>
-              <CardFooter><Button className='w-full'>مشاهده بیشتر</Button></CardFooter>
+              <CardContent className="absolute bottom-0 right-0 left-0 top-49 bg-white rounded-t-4xl flex flex-col justify-between p-4 group-hover:top-0 transition-all group-hover:backdrop-blur-3xl group-hover:bg-white/50">
+                <div className="p-2 space-y-12">
+                  <CardTitle className="text-2xl font-normal">{n.title}</CardTitle>
+                  <CardDescription className="text-gray-700">{n.description}</CardDescription>
+                </div>
+                <CardFooter className="p-0">
+                  <Button className="w-full rounded-full">مشاهده بیشتر</Button>
+                </CardFooter>
+              </CardContent>
             </Card>
           ))}
         </div>
