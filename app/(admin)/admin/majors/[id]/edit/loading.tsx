9d848872729dab18f6d6799f944224264875165a
sadra <mmdsadra88@gmail.com>
2026-09-13 11:21:58 -0400

    Refactor : fix issue in loading edit page

--- app/(admin)/admin/majors/[id]/edit/loading.tsx
+++ app/(admin)/admin/majors/[id]/edit/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 const EditLoadingPage = () => {
   return (
-    <div className="flex w-full max-w-xl min-h-xl flex-col gap-18">
+    <div className="flex w-full max-w-xl min-h-xl flex-col gap-18 mx-auto">
       <div className="flex flex-col gap-3">
         <Skeleton className="h-4 w-20 bg-gray-200" />
         <Skeleton className="h-8 w-full bg-gray-200" />
@@ -11,6 +11,7 @@ const EditLoadingPage = () => {
         <Skeleton className="h-4 w-24 bg-gray-200" />
         <Skeleton className="h-70 w-full bg-gray-200" />
       </div>
+      <Skeleton className="h-40 w-full bg-gray-200" />
       <Skeleton className="h-8 w-full bg-gray-200" />
     </div>
   )
