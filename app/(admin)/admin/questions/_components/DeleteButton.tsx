ad4860b6a24e41d4558d6e9c50e9ebd91005c823
sadra <mmdsadra88@gmail.com>
2026-09-15 11:56:44 -0400

    Create delete button component

---
+++ app/(admin)/admin/questions/_components/DeleteButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const DeleteButton = () => {
+  return (
+    <Button variant="destructive">حذف سوال</Button>
+  )
+};
+
+export default DeleteButton;
--- app/(admin)/admin/questions/page.tsx
+++ app/(admin)/admin/questions/page.tsx
@@ -1,33 +1,35 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { prisma } from "@/prisma/client";
-import Markdown from "react-markdown";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { prisma } from "@/prisma/client";
+import Markdown from "react-markdown";
+import DeleteButton from "./_components/DeleteButton";
 
 const QuestionsPage = async () => {
   const questions = await prisma.question.findMany();
-  
+
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 gap-4">
-        {questions.map(q => (
+        {questions.map((q) => (
           <Card key={q.id} className="p-4 wrap-break-word">
-            <CardHeader className="bg-primary/50 border-2 border-primary rounded-xl p-2">{q.title}</CardHeader>
+            <CardHeader className="bg-primary/50 border-2 border-primary rounded-xl p-2">
+              {q.title}
+            </CardHeader>
             <CardContent className="bg-blue-superlight border-2 border-secondary rounded-xl p-2">
-              <Markdown>{q.anwser.length > 60 ? `${q.anwser.substring(0, 60)}...` : q.anwser}</Markdown>
+              <Markdown>
+                {q.anwser.length > 60
+                  ? `${q.anwser.substring(0, 60)}...`
+                  : q.anwser}
+              </Markdown>
               <DropdownMenu>
                 <DropdownMenuTrigger render={<Button variant="outline" />}>
                   بیشتر
                 </DropdownMenuTrigger>
-              
                 <DropdownMenuContent className="w-100">
                   <div className="max-h-100 overflow-y-auto p-3 text-sm whitespace-normal break-words [overflow-wrap:anywhere]">
                     <Markdown>{q.anwser}</Markdown>
@@ -35,11 +37,14 @@ const QuestionsPage = async () => {
                 </DropdownMenuContent>
               </DropdownMenu>
             </CardContent>
+            <CardFooter className="p-0">
+              <DeleteButton />
+            </CardFooter>
           </Card>
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default QuestionsPage;
