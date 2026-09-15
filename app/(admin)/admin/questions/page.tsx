9b69ab25198328e6559966c08f442984cff3d997
sadra <mmdsadra88@gmail.com>
2026-09-15 11:37:05 -0400

    Show questions

--- app/(admin)/admin/questions/page.tsx
+++ app/(admin)/admin/questions/page.tsx
@@ -1,6 +1,23 @@
-const QuestionsPage = () => {
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { prisma } from "@/prisma/client";
+import Markdown from "react-markdown";
+
+const QuestionsPage = async () => {
+  const questions = await prisma.question.findMany();
+  
   return (
-    <>Questions</>
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {questions.map(q => (
+          <Card key={q.id} className="p-4 wrap-break-word">
+            <CardHeader className="bg-primary/50 border-2 border-primary rounded-xl p-2">{q.title}</CardHeader>
+            <CardContent className="bg-blue-superlight border-2 border-secondary rounded-xl p-2">
+              <Markdown>{q.anwser.length > 60 ? `${q.anwser.substring(0 , 60)}...` : q.anwser}</Markdown>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </section>
   )
 }
 
