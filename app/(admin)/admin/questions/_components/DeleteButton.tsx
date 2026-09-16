bf6f9af4664e4d8a99ab6c28bdca2751fc67f8f0
sadra <mmdsadra88@gmail.com>
2026-09-16 09:44:49 -0400

    Handle delete question errors

--- app/(admin)/admin/questions/_components/DeleteButton.tsx
+++ app/(admin)/admin/questions/_components/DeleteButton.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { deleteQuestion } from "../actions";
 import { useState } from "react";
 import { Spinner } from "@/components/ui/spinner";
+import { toast } from "sonner";
 
 const DeleteButton = ({ id }: { id: number }) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -12,8 +13,10 @@ const DeleteButton = ({ id }: { id: number }) => {
     try {
       setIsLoading(true);
       await deleteQuestion(id);
+      toast.success('سوال با موفقیت حذف شد');
     } catch (error) {
       console.log(error);
+      toast.error('سوال حذف نشد');
     } finally {
       setIsLoading(false);
     }
