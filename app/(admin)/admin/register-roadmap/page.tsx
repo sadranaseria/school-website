b4bc5f09f662408cebeb3ca9f476b347a45614e4
sadra <mmdsadra88@gmail.com>
2026-09-17 11:21:52 -0400

    Create roadmap box

--- app/(admin)/admin/register-roadmap/page.tsx
+++ app/(admin)/admin/register-roadmap/page.tsx
@@ -1,8 +1,11 @@
 const RegisterRoadmapPage = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="max-w-sm mx-auto">
+      <div className="w-full h-20 border-2 border-primary text-center rounded-lg flex items-center justify-center">
+        Something
+      </div>
+      <div className="w-0.5 bg-primary h-20 rounded-full mx-auto"></div>
+    </section>
   );
 };
 
