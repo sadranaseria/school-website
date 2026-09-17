3ebac87cec64a5ad17a902ef6f70ce18842420b2
sadra <mmdsadra88@gmail.com>
2026-09-17 13:13:20 -0400

    Handle deleting step loading

--- app/(admin)/admin/register-roadmap/DeleteStepButton.tsx
+++ app/(admin)/admin/register-roadmap/DeleteStepButton.tsx
@@ -1,19 +1,33 @@
+'use client';
+
 import { Button } from "@/components/ui/button";
 import { deleteRoadmapStep } from "./actions";
+import { useState } from "react";
+import { Spinner } from "@/components/ui/spinner";
 
 const DeleteStepButton = ({ stepId }: { stepId: number }) => {
+  const [isLoading , setLoading] = useState(false);
+  
   const hanldeDelete = async () => {
     try {
+      setLoading(true);
       await deleteRoadmapStep(stepId);
     } catch (error) {
       console.log(error);
     } finally {
-      
+      setLoading(false);
     }
   }
   
   return (
-    <Button variant="destructive" onClick={hanldeDelete}>حذف مرحله</Button>
+    <Button variant="destructive" onClick={hanldeDelete}>{isLoading ? (
+      <div className="flex items-center gap-2">
+        <Spinner />
+    در حال حذف
+      </div>
+    ) : (
+'حذف مرحله'
+    )}</Button>
   )
 }
 
