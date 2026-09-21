f2c086b5758fdd9be5c7ce64138ab2f7a14196c8
sadra <mmdsadra88@gmail.com>
2026-09-21 10:55:10 -0400

    Refactor : redesign the question section

--- app/(client)/QuestionsSection.tsx
+++ app/(client)/QuestionsSection.tsx
@@ -9,16 +9,18 @@ const QuestionsSection = async () => {
   return (
     <ObserverProvider id="questions">
       <SectionTitle title="سوالات متداول" href="#questions" />
-      <div className="text-center space-y-2">
-        <h2 className="text-2xl md:text-6xl text-secondary font-bold">
-          به پرسش های شما پاسخ می‌دهیم
-        </h2>
-        <p className="text-lg md:text-4xl text-secondary leading-8 md:leading-14 font-extralight">
-          در این بخش می‌توانید پاسخ سوالات رایج درباره هنرستان و خدمات آموزشی ما
-          را مشاهده کنید
-        </p>
-      </div>
-      <QusetionAccordion questions={questions} />
+      <section className="flex gap-20">
+        <div className="text-center space-y-2">
+          <h2 className="text-2xl md:text-6xl text-secondary font-bold">
+            به پرسش های شما پاسخ می‌دهیم
+          </h2>
+          <p className="text-lg md:text-4xl text-secondary leading-8 md:leading-14 font-extralight">
+            در این بخش می‌توانید پاسخ سوالات رایج درباره هنرستان و خدمات آموزشی ما
+            را مشاهده کنید
+          </p>
+        </div>
+        <QusetionAccordion questions={questions} />
+      </section>
     </ObserverProvider>
   );
 };
--- app/(client)/QusetionAccordion.tsx
+++ app/(client)/QusetionAccordion.tsx
@@ -12,11 +12,11 @@ const QusetionAccordion = ({ questions }: { questions: Question[] }) => {
   return (
     <Accordion
       dir="rtl"
-      className="w-full p-4 space-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-start gap-4 my-12"
+      className='w-full divide-y divide-primary ring-2 ring-primary rounded-2xl p-4'
       defaultValue={["billing"]}
     >
-      {questions.map((q , i) => (
-        <AccordionItem className="border-2 border-thertiary p-1 md:p-2 rounded-3xl" key={q.id}>
+      {questions.map((q, i) => (
+        <AccordionItem className="" key={q.id}>
           <AccordionTrigger className="flex items-center gap-4 hover:underline-offset-2 decoration-blue-superlight">
             <div className="flex items-center gap-2">
               <p className="size-8 md:size-7 flex justify-center items-center bg-blue-superlight text-secondary text-sm md:text-md rounded-full pt-1">
